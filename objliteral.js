d6c1f92a434da76affdd52459b0652a359402b69
juan sacco <juandsacco@gmail.com>
2022-10-31 21:20:38 -0300

    creo nuevo repositorio, 3ra entrega proyecto final js

---
+++ objliteral.js
@@ -0,0 +1,37 @@
+const productos = [{
+    id: 1,
+    foto: "img/pc.jpg" ,
+    nombre: "Pc Escritorio",
+    precio: 60000
+},
+{
+    id: 2,
+    foto: "img/placa.jpg",
+    nombre: "Placa de Video ASUS",
+    precio: 40000
+},
+{
+    id: 3,
+    foto:"img/auricular.jpg" ,
+    nombre: "Auricular XBOX",
+    precio: 10000
+},
+{
+    id: 4,
+    foto: "img/mouse.jpg" ,
+    nombre: "Mouse HyperX",
+    precio: 8000
+},
+{
+    id: 5,
+    foto: "img/ram.jpg" ,
+    nombre: "Memoria Ram 16GB",
+    precio: 4000
+},
+{
+    id: 6,
+    foto: "img/setup.jpg" ,
+    nombre: "Set Up Streamer/Gamer",
+    precio: 20000
+}
+];
